72d22d396efc08375aebb9f57d26dd6081f8e2db
Blake Worsley <blakeworsley@gmail.com>
2018-03-24 08:03:51 -0600

    Adds sass to project

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../styles/root.css';
+import '../styles/root.scss';
 
 class Root extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
+import './styles/index.scss';
 import Root from './components/Root';
 import registerServiceWorker from './utils/registerServiceWorker';
 
